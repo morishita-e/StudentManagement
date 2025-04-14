83cd3e1221ac85d1db1223ea88ef2eb6d2d8c32d
morishita-e <l1812369968as@gmail.com>
2025-04-14 17:03:44 +0900

    スタンダート第22回演習課題

--- src/main/java/raisetech/StudentManagement/data/Student.java
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -45,5 +45,6 @@ public class Student {
   @Size(max = 100, message = "備考は100文字以内で入力してください")
   private String remark;
   private Boolean isDeleted = false; // デフォルト値を false に設定
+
 }
 
--- src/main/java/raisetech/StudentManagement/data/StudentCourse.java
+++ src/main/java/raisetech/StudentManagement/data/StudentCourse.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 public class StudentCourse {
